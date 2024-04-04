fb6ab398eb332cb2201ee3059b199d2e66461d49
arrovain <arrovain@gmail.com>
2024-04-04 14:01:54 +0300

    Building Hero, Navbar and Higlight components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0);
+import * as Sentry from "@sentry/react";
 
-  return <div className="text-3xl font-bold underline"></div>;
-}
-
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  );
+};
 export default App;
